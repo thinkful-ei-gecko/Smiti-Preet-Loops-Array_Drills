95e2cd6040b532d6ca7fe3d39251c74ca78627f8
github username <github email address>
2019-08-13 11:14:23 -0500

    update with max and min

---
+++ loops.js
@@ -0,0 +1,27 @@
+'use strict'
+
+function max(numbers){
+    let maxVal = numbers[0];
+    let i = 0;
+    while (i < numbers.length){
+        if (numbers[i] > maxVal){
+            maxVal = numbers[i];
+        }
+        i++;
+    }
+    return maxVal;
+}
+
+function min(numbers){
+    let minVal = numbers[0];
+    let i = 0;
+    while (i < numbers.length){
+        if (numbers[i] < minVal){
+            minVal = numbers[i];
+        }
+        i++;
+    }
+    return minVal;
+}
+
+console.log(max([7, 8, 9, 99]));
